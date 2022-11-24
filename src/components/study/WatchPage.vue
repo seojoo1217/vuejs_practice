e8637123d2bbf6c750c28e149fd9165e6e568f25
sjlee <sjlee@raoncorp.com>
2022-11-24 14:50:06 +0900

    문장 정렬

--- src/components/study/WatchPage.vue
+++ src/components/study/WatchPage.vue
@@ -21,21 +21,33 @@ export default {
   },
 };
 </script>
-
+//
 <template>
-  <div>
-    computed/watch 두 방법의 접근방식은 서로 동일하다
-    <br />
-    computed = 의존하는 값이 변하는 경우 계속 실행 &nbsp; <br />
-    함수가 실행된다 <br />
-    method = 값이 렌더링 된 값으로 유지 하고 싶으면 method
-    <br />
-    computed = 계속 캐싱하여 값을 바꿔 보여주고 싶을땐 computed
+  <div class="container">
+    <div class="info">
+      computed/watch 두 방법의 접근방식은 서로 <b>동일</b>하다
+      <br />
+      computed = 의존하는 값이 변하는 경우 계속 실행 함수가 실행된다
+      <br />
+      <hr />
+      method = 값이 렌더링 된 값으로 유지 하고 싶으면 method
+      <br />
+      computed = 계속 캐싱하여 값을 바꿔 보여주고 싶을땐 computed
+    </div>
   </div>
-
-  Computed = {{ publishedBooksMessage }}
-  {{ author.books }}
 </template>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped></style>
+<style scoped>
+.container {
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: start;
+}
+.info {
+  width: 80%;
+  padding: 20px;
+  text-align: left;
+}
+</style>
