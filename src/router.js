06c3d8b5625f396fa3c81a34c42ebfe0d64f99dd
sjlee <sjlee@raoncorp.com>
2022-12-06 18:22:22 +0900

    test

--- src/router.js
+++ src/router.js
@@ -1,7 +1,7 @@
-import TodoPage from "./components/study/TodoPage.vue";
-import RoutesPage from "./components/study/RoutesPage.vue";
-import WatchPage from "./components/study/WatchPage.vue";
-import BodyPage from "./components/page/BodyPage.vue";
+// import TodoPage from "./components/study/TodoPage.vue";
+// import RoutesPage from "./components/study/RoutesPage.vue";
+// import WatchPage from "./components/study/WatchPage.vue";
+// import BodyPage from "./components/page/BodyPage.vue";
 
 ///
 import ContentsPage from "./components/shopping/common/Contents.vue";
@@ -10,17 +10,17 @@ import { createWebHistory, createRouter } from "vue-router";
 import DetailPage from "./components/shopping/page/ProductDetailPage.vue";
 //
 const routes = [
+  // {
+  //   path: "/vuejs_practice",
+  //   component: BodyPage,
+  //   children: [
+  //     { path: "", name: "todoPage", component: TodoPage },
+  //     { path: "routes", name: "routesPage", component: RoutesPage },
+  //     { path: "watch", name: "watchPage", component: WatchPage },
+  //   ],
+  // },
   {
     path: "/vuejs_practice",
-    component: BodyPage,
-    children: [
-      { path: "", name: "todoPage", component: TodoPage },
-      { path: "routes", name: "routesPage", component: RoutesPage },
-      { path: "watch", name: "watchPage", component: WatchPage },
-    ],
-  },
-  {
-    path: "/clone_coding",
     component: ContentsPage,
     children: [
       { path: "main", name: "mainPage", component: MainPage },
